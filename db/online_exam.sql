c24266e711b0ad2cd1b10125136c132a19ae1c28
prathapswathi <4dm15cs041.swathi@gmail.com>
2019-11-21 16:06:35 +0530

    adding updated sql file

--- db/online_exam.sql
+++ db/online_exam.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 20, 2019 at 04:24 PM
+-- Generation Time: Nov 21, 2019 at 04:05 PM
 -- Server version: 5.7.27-0ubuntu0.16.04.1
 -- PHP Version: 7.0.33-0ubuntu0.16.04.7
 
@@ -32,17 +32,18 @@ CREATE TABLE `register` (
   `lastname` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(250) NOT NULL,
-  `user_type` varchar(50) NOT NULL
+  `user_type` varchar(50) NOT NULL,
+  `last_activity` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `register`
 --
 
-INSERT INTO `register` (`id`, `firstname`, `lastname`, `email`, `password`, `user_type`) VALUES
-(5, 'apeksha', 'hegde', 'apeksha@gmail.com', 'appi123', 'user'),
-(8, 'anju', 'vijayan', 'anju@impelsys.com', 'anju123', 'user'),
-(10, 'admin', '', 'admin@impelsys.com', 'admin@123', 'admin');
+INSERT INTO `register` (`id`, `firstname`, `lastname`, `email`, `password`, `user_type`, `last_activity`) VALUES
+(1, 'admin', '', 'admin@impelsys.com', 'admin@123', 'admin', '2019-11-21 12:05:16'),
+(12, 'sujnan', 'prasad', 'sujnan@impelsys.com', '$2y$10$RoLQ/IEJLGP.gp/.2SPTh.3Io0Yw0QOE1VAPmbGwbcakvdI4CuNoi', 'user', '0000-00-00 00:00:00'),
+(16, 'afreen', 'v', 'afreen@impelsys.com', '$2y$10$u.Khcjfyryh8L5HHXVuDE.T7c8BESiXF/qw8fcUI15lEDP5EsN04O', 'user', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -52,7 +53,7 @@ INSERT INTO `register` (`id`, `firstname`, `lastname`, `email`, `password`, `use
 
 CREATE TABLE `topics` (
   `id` int(11) NOT NULL,
-  `course_id` int(11) NOT NULL,
+  `course_id` varchar(50) NOT NULL,
   `course_name` varchar(50) NOT NULL,
   `description` varchar(50) NOT NULL,
   `image` blob NOT NULL
@@ -83,7 +84,7 @@ ALTER TABLE `topics`
 -- AUTO_INCREMENT for table `register`
 --
 ALTER TABLE `register`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `topics`
 --
